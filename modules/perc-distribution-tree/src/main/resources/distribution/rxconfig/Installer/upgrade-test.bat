e53cb892c37f46fcf02e61ea044414707bfaff97
Santosh Dhariwal <sonydeswal@yahoo.com>
2021-01-27 20:57:52 +0530

    Updating <CmsVersion max="9.0.0"..for psx_archiveInfo.xml for all widgets

--- modules/perc-distribution-tree/src/main/resources/distribution/rxconfig/Installer/upgrade-test.bat
+++ modules/perc-distribution-tree/src/main/resources/distribution/rxconfig/Installer/upgrade-test.bat
@@ -1,1 +1,1 @@
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8050 -Dinstall.dir=C:\CM5315 -Dinstall.stage.dts=false -Dinstall.dts=false -jar perc-ant-8.0.0-SNAPSHOT.jar -f install.xml
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8050 -Dinstall.dir=C:\CM5315 -Dinstall.stage.dts=false -Dinstall.dts=false -jar perc-ant-8.0.2-SNAPSHOT.jar -f install.xml
