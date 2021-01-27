64c3a1c37a7c64d3e11bd0bea82dd5fbee6ff58c
Santosh Dhariwal <sonydeswal@yahoo.com>
2021-01-27 20:57:52 +0530

    Updating <CmsVersion max="9.0.0"..for psx_archiveInfo.xml for all widgets

--- modules/perc-distribution-tree/UpdateTinyMCE.bat
+++ modules/perc-distribution-tree/UpdateTinyMCE.bat
@@ -4,6 +4,6 @@ cd ..\..\
 start /WAIT /B cmd /c mvn clean install -DskipTests=true -pl :perc-tinymce
 cd modules\perc-distribution-tree\
 @echo Updating TinyMCE..
-copy /Y ..\perc-tinymce\target\perc-tinymce-8.0.0-SNAPSHOT.jar target\classes\distribution\jetty\base\webapps\Rhythmyx\WEB-INF\lib
+copy /Y ..\perc-tinymce\target\perc-tinymce-8.0.2-SNAPSHOT.jar target\classes\distribution\jetty\base\webapps\Rhythmyx\WEB-INF\lib
 @echo Starting Jetty...
 start /WAIT cmd /C target\classes\distribution\jetty\StartJetty.bat
