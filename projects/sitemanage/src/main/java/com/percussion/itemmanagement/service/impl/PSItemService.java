78f006e9664665de70561939ecf1fe2cff4e27e4
Nate Chadwick <nate.chadwick@gmail.com>
2021-02-19 03:08:56 -0500

    Refactor methods to check exceptions.

--- projects/sitemanage/src/main/java/com/percussion/itemmanagement/service/impl/PSItemService.java
+++ projects/sitemanage/src/main/java/com/percussion/itemmanagement/service/impl/PSItemService.java
@@ -1,6 +1,6 @@
 /*
  *     Percussion CMS
- *     Copyright (C) 1999-2020 Percussion Software, Inc.
+ *     Copyright (C) 1999-2021 Percussion Software, Inc.
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
@@ -399,19 +399,22 @@ public class PSItemService implements IPSItemService
                 try{
                 	impact.getOwnerPages().add(folderHelper.findItemPropertiesById(page));
                 }catch(Exception e){
-                	log.error("Error processing Site Impact with owner Page " + page + " for Asset id:" + id, e);
+                	log.error("Error processing Site Impact with owner Page {} for Asset id: {} Error: {}",page, id, e.getMessage());
+                	log.debug(e.getMessage(),e);
                 }
 
                 List<IPSSite> sites = new ArrayList<>();
                 try{
                 	sites = folderHelper.getItemSites(page);
                 }catch(Exception e){
-                	log.error("Error processing Site Impact detecting owner Site for Page " + page + " for Asset id:" + id, e);
+                	log.error("Error processing Site Impact detecting owner Site for Page {} for Asset id: {} Error: {}",
+                             page,
+                             id,
+                             e.getMessage());
+                	log.debug(e.getMessage(),e);
                 }
 
-                for(IPSSite site : sites){
-                	ownerSites.add(site);
-                }
+                ownerSites.addAll(sites);
             }
 
 			for (String templateId : ownerTemplates){
