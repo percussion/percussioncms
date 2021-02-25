c6674be8761766266eda98fb4ec9581cfb7d1e68
Nate Chadwick <nate.chadwick@gmail.com>
2021-02-25 11:15:15 -0500

    Increase sleep as test intermittently fails.

--- deliverytiersuite/delivery-tier-suite/metadata/src/test/java/com/percussion/delivery/metadata/PSMostReadServiceTest.java
+++ deliverytiersuite/delivery-tier-suite/metadata/src/test/java/com/percussion/delivery/metadata/PSMostReadServiceTest.java
@@ -1,6 +1,6 @@
 /*
  *     Percussion CMS
- *     Copyright (C) 1999-2020 Percussion Software, Inc.
+ *     Copyright (C) 1999-2021 Percussion Software, Inc.
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
@@ -27,7 +27,6 @@ package com.percussion.delivery.metadata;
 import com.percussion.delivery.metadata.data.PSVisitQuery;
 import com.percussion.delivery.metadata.rdbms.impl.PSDbBlogPostVisit;
 import com.percussion.delivery.metadata.rdbms.impl.PSDbMetadataEntry;
-import com.percussion.delivery.test.TestMultiConcurrentCallsToServer;
 import junit.framework.TestCase;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -121,16 +120,16 @@ public class PSMostReadServiceTest extends TestCase {
     	List<String> topPages = blogPostService.getTopVisitedBlogPosts(query);
     	
         assertTrue("most read pages not empty", topPages.size() > 0);
-        assertTrue("size is equal to 3", topPages.size() == 3);
+		assertEquals("size is equal to 3", 3, topPages.size());
         
         // hits should be returned based on last entry date as there is no more than 1
         // entry for each page in DB at this time.
         String testName = PAGE_FULL + "4.html";
-        assertTrue("first item in list was last item tracked", testName.equals(topPages.get(0)));
+		assertEquals("first item in list was last item tracked", testName, topPages.get(0));
         
         // page 2 should be last item in list because we limited the query by 3
         testName = PAGE_FULL + "2.html";
-        assertTrue("page2.html should be returned", testName.equals(topPages.get(2)));
+		assertEquals("page2.html should be returned", testName, topPages.get(2));
     }
     
     /**
@@ -152,17 +151,17 @@ public class PSMostReadServiceTest extends TestCase {
     	mixUpBlogPostHits(topPages);
     	
     	topPages = blogPostService.getTopVisitedBlogPosts(query);
-    	
-    	assertTrue("list should contain 5 items", topPages.size() == 5);
+
+		assertEquals("list should contain 5 items", 5, topPages.size());
     	
     	String testName = PAGE_FULL + "0.html";
-    	assertTrue("page0.html should have the most hits", testName.equals(topPages.get(4)));
+		assertEquals("page0.html should have the most hits", testName, topPages.get(4));
     	
     	testName = PAGE_FULL + "2.html";
-    	assertTrue("page2.html should have 2nd most hits", testName.equals(topPages.get(2)));
+		assertEquals("page2.html should have 2nd most hits", testName, topPages.get(2));
     	
     	testName = PAGE_FULL + "1.html";
-    	assertTrue("page1.html should have 3rd most hits", testName.equals(topPages.get(3)));
+		assertEquals("page1.html should have 3rd most hits", testName, topPages.get(3));
     	
     }
     
@@ -180,8 +179,8 @@ public class PSMostReadServiceTest extends TestCase {
     	query.setTimePeriod("ALLTIME");
     	
     	List<String> pagePaths = blogPostService.getTopVisitedBlogPosts(query);
-    	
-    	assertTrue("Page paths size should be 0", pagePaths.size() == 0);
+
+		assertEquals("Page paths size should be 0", 0, pagePaths.size());
     }
     
     @Test
@@ -193,11 +192,11 @@ public class PSMostReadServiceTest extends TestCase {
     	query.setTimePeriod("ALLTIME");
     	
     	List<String> pagePaths = blogPostService.getTopVisitedBlogPosts(query);
-    	
-    	assertTrue("list size should be 1", pagePaths.size() == 1);
+
+		assertEquals("list size should be 1", 1, pagePaths.size());
     	
     	String testName = PAGE_FULL + "4.html";
-    	assertTrue("least hit page should be page4.html", testName.equals(pagePaths.get(0)));
+		assertEquals("least hit page should be page4.html", testName, pagePaths.get(0));
     	
     	query.setSortOrder("desc");
     	pagePaths = blogPostService.getTopVisitedBlogPosts(query);
@@ -219,14 +218,14 @@ public class PSMostReadServiceTest extends TestCase {
     	query.setTimePeriod("ALLTIME");
     	
     	List<String> topPosts = blogPostService.getTopVisitedBlogPosts(query);
-    	
-    	assertTrue("List size is 25", topPosts.size() == 25);
+
+		assertEquals("List size is 25", 25, topPosts.size());
     	
     	// ensure 155 still got set regardless of UI max limit
     	query.setLimit("155");
     	
     	topPosts = blogPostService.getTopVisitedBlogPosts(query);
-    	assertTrue("List size is 155", topPosts.size() == 155);
+		assertEquals("List size is 155", 155, topPosts.size());
     }
     
     @Test
@@ -255,9 +254,7 @@ public class PSMostReadServiceTest extends TestCase {
     	catch (Exception e) {
     		assertTrue("delete method should be unsupported", e instanceof UnsupportedOperationException);
     	}
-    	
-    	// pagePaths = blogPostService.getTopVisitedBlogPosts(query);
-    	// assertTrue("size should be back to 5", pagePaths.size() == 5);
+
     }
     
     @Test
@@ -317,7 +314,8 @@ public class PSMostReadServiceTest extends TestCase {
 
         // sleep for 2 seconds, 1 second longer than PSBlogPostVisitService
         // thread executor scheduler
-    	Thread.sleep(2000);
+		//TODO: Figure out why this is needed.
+    	Thread.sleep(5000);
     }
     
     /**
@@ -368,7 +366,8 @@ public class PSMostReadServiceTest extends TestCase {
 
         // sleep for 2 seconds, 1 second longer than PSBlogPostVisitService
         // thread executor scheduler
-    	Thread.sleep(2000);
+		//TODO: Figure out why this is needed.
+    	Thread.sleep(5000);
     }
     
     
