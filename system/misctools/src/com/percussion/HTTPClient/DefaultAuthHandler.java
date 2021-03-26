bd2c05583024fb6be5e70e054fdef12add7dfb8d
Nate Chadwick <nate.chadwick@gmail.com>
2021-03-26 10:04:31 -0400

    Remove explicit GC

--- system/misctools/src/com/percussion/HTTPClient/DefaultAuthHandler.java
+++ system/misctools/src/com/percussion/HTTPClient/DefaultAuthHandler.java
@@ -1,6 +1,6 @@
 /*
  *     Percussion CMS
- *     Copyright (C) 1999-2020 Percussion Software, Inc.
+ *     Copyright (C) 1999-2021 Percussion Software, Inc.
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
@@ -24,25 +24,12 @@
 
 package com.percussion.HTTPClient;
 
-import java.awt.BorderLayout;
-import java.awt.Button;
-import java.awt.Dimension;
-import java.awt.Frame;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.awt.Label;
-import java.awt.Panel;
-import java.awt.TextField;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 import java.util.Vector;
 
@@ -201,15 +188,6 @@ public class DefaultAuthHandler implements AuthorizationHandler, GlobalConstants
 					 challenge.getRealm(),
 					 upwd, null);
 	}
-
-
-	// try to get rid of any unencoded passwords in memory
-
-	answer = null;
-	System.gc();
-
-
-	// Done
 
 	Log.write(Log.AUTH, "Auth:  Got Authorization");
 
