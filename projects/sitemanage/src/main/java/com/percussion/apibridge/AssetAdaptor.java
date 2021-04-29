8dd1d604a69ab85800f27018c8e789e7b9419b80
malabika.baguli@gmail.com <malabika.baguli@gmail.com>
2021-04-28 22:50:24 -0400

    fixes to Java file

--- projects/sitemanage/src/main/java/com/percussion/apibridge/AssetAdaptor.java
+++ projects/sitemanage/src/main/java/com/percussion/apibridge/AssetAdaptor.java
@@ -3,7 +3,8 @@
  *     Copyright (C) 1999-2020 Percussion Software, Inc.
  *
  *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *     it under the terms of the GNU Affero General Public License as published by the Free Software Foundation,
+ *     either version 3 of the License, or (at your option) any later version.
  *
  *     This program is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
