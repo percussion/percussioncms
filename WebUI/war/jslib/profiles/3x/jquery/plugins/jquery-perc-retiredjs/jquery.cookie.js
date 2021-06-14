6172d5ecdd896c1f1f24dcb1c040bbc0cb8324da
Santosh Dhariwal <sonydeswal@yahoo.com>
2021-06-14 19:23:38 +0530

    Missing Icons

--- WebUI/war/jslib/profiles/3x/jquery/plugins/jquery-perc-retiredjs/jquery.cookie.js
+++ WebUI/war/jslib/profiles/3x/jquery/plugins/jquery-perc-retiredjs/jquery.cookie.js
@@ -83,7 +83,7 @@ jQuery.cookie = function(name, value, options) {
         if (document.cookie && document.cookie != '') {
             var cookies = document.cookie.split(';');
             for (var i = 0; i < cookies.length; i++) {
-                var cookie = String.prototype.trim(cookies[i]);
+                var cookie = jQuery.trim(cookies[i]);
                 // Does this cookie string begin with the name we want?
                 if (cookie.substring(0, name.length + 1) == (name + '=')) {
                     cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
