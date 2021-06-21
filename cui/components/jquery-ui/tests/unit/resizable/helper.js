ee20f33c5205ebfaea63de49a0acf2052196ebd0
Nate Chadwick <nate.chadwick@gmail.com>
2021-06-21 12:14:14 -0400

    Updates for home screen for jQuery 3

---
+++ cui/components/jquery-ui/tests/unit/resizable/helper.js
@@ -0,0 +1,21 @@
+define( [
+	"qunit",
+	"jquery",
+	"lib/helper"
+], function( QUnit, $, helper ) {
+"use strict";
+
+return $.extend( helper, {
+	drag: function( el, dx, dy ) {
+
+		// This mouseover is to work around a limitation in resizable
+		// TODO: fix resizable so handle doesn't require mouseover in order to be used
+		$( el ).simulate( "mouseover" ).simulate( "drag", {
+			moves: 2,
+			dx: dx,
+			dy: dy
+		} );
+	}
+} );
+
+} );
