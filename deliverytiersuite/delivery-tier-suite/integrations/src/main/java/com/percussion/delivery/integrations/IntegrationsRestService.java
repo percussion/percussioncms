6a1bea9004baa2d9ff37afc084af9446ce59d8e3
Nate Chadwick <nate.chadwick@gmail.com>
2021-07-27 14:31:53 -0400

    CMS-8253 - Resolve

---
+++ deliverytiersuite/delivery-tier-suite/integrations/src/main/java/com/percussion/delivery/integrations/IntegrationsRestService.java
@@ -0,0 +1,60 @@
+/*
+ *     Percussion CMS
+ *     Copyright (C) 1999-2021 Percussion Software, Inc.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     Mailing Address:
+ *
+ *      Percussion Software, Inc.
+ *      PO Box 767
+ *      Burlington, MA 01803, USA
+ *      +01-781-438-9900
+ *      support@percussion.com
+ *      https://www.percussion.com
+ *
+ *     You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.percussion.delivery.integrations;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.HEAD;
+import javax.ws.rs.Path;
+
+@Path("/integrations")
+@Component
+public class IntegrationsRestService {
+
+    public IntegrationsRestService(){
+        //NOOP
+    }
+
+    @Inject
+    HttpServletRequest request;
+
+    @Inject
+    HttpServletResponse response;
+
+    @HEAD
+    @Path("/csrf")
+    public void csrf()  {
+        CsrfToken csrfToken = new HttpSessionCsrfTokenRepository().loadToken(request);
+
+        response.setHeader("X-CSRF-HEADER", csrfToken.getHeaderName());
+        response.setHeader("X-CSRF-PARAM", csrfToken.getParameterName());
+        response.setHeader("X-CSRF-TOKEN", csrfToken.getToken());
+    }
+}
