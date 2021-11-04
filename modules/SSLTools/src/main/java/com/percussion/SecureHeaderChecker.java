02a9271e3b972e422918eac4eaef56f0831eec1e
Nate Chadwick <nate.chadwick@gmail.com>
2021-11-04 14:53:23 -0400

    Add support for checking secure headers too.

---
+++ modules/SSLTools/src/main/java/com/percussion/SecureHeaderChecker.java
@@ -0,0 +1,39 @@
+package com.percussion;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class SecureHeaderChecker {
+
+    private static final String[] secureHeaders = {
+            "X-Frame-Options",
+            "Content-Security-Policy",
+            "X-Content-Type-Options",
+            "Strict-Transport-Security",
+            "X-XSS-Protection",
+            "Cache-Control"
+    };
+
+
+    /**
+     * Checks the connection for the presence of secure headers and
+     * @param conn
+     * @return
+     */
+    public static SecureHeaderCheckResponse check(HttpsURLConnection conn) {
+
+        SecureHeaderCheckResponse response = new SecureHeaderCheckResponse();
+
+        for(String h : secureHeaders){
+            String result = conn.getHeaderField(h);
+            if(result == null){
+                response.getChecks().put(h,false);
+                response.setFailedCheck(true);
+            }else{
+                response.getChecks().put(h,true);
+            }
+        }
+        return response;
+    }
+
+
+}
