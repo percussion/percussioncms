f3cc22ebcfb4f48af236ed5f2ad5b5f393d96166
Santosh R Dhariwal <sonydeswal@yahoo.com>
2021-12-09 19:53:58 +0530

    Copy should Copy Site not folder

--- cui/components/knockoutjs/build/fragments/extern-pre.js
+++ cui/components/knockoutjs/build/fragments/extern-pre.js
@@ -1,4 +1,4 @@
-(function(undefined){
+(function(undefined) {
     // (0, eval)('this') is a robust way of getting a reference to the global object
     // For details, see http://stackoverflow.com/questions/14119988/return-this-0-evalthis/14120023#14120023
     var window = this || (0, eval)('this'),
@@ -6,3 +6,4 @@
         navigator = window['navigator'],
         jQuery = window["jQuery"],
         JSON = window["JSON"];
+})
