f303230d7ea081a7614a03be13d06a9a988a7965
Vineet <vineetpeeyuse@gmail.com>
2022-01-05 16:42:17 +0530

    CMS-8706 : Server error page appeared in case form submission failed instead of form error page.

--- deliverytiersuite/delivery-tier-suite/common/src/main/java/com/percussion/delivery/exceptions/PSUncaughtError.java
+++ deliverytiersuite/delivery-tier-suite/common/src/main/java/com/percussion/delivery/exceptions/PSUncaughtError.java
@@ -24,18 +24,53 @@
 
 package com.percussion.delivery.exceptions;
 
+import com.percussion.error.PSExceptionUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.net.URL;
 
 @Provider
 public class PSUncaughtError extends Throwable implements ExceptionMapper<Throwable>
 {
     private static final long serialVersionUID = 1L;
 
+    private  static final Logger log = LogManager.getLogger(PSUncaughtError.class);
+
+    @Context private HttpServletRequest request;
+
+    @Context private HttpServletResponse response;
+
     @Override
     public Response toResponse(Throwable exception)
     {
-        return Response.status(500).entity("A server error happened. Please try your request again.").type("text/plain").build();
+        try {
+            log.warn("Page redirecting to error {} : ", request.getHeader("referer"));
+            log.warn("Error code {} : ", response.getStatus());
+            log.warn("Error message {} : ", exception.getLocalizedMessage());
+            String referer = request.getHeader("referer");
+            URL url = new URL(referer);
+            String hostRedirect = url.getHost();
+            Integer port = url.getPort();
+            String errorRedirect = "";
+            if(port != null && port>0){
+                errorRedirect = request.getScheme()+"://"+hostRedirect+":"+port+"/error.html";
+            }else{
+                errorRedirect = request.getScheme()+"://"+hostRedirect+"/error.html";
+            }
+            response.sendRedirect(errorRedirect);
+        } catch (IOException e) {
+            log.error("Exception occurred while redirecting to error.html, Error: {}", PSExceptionUtils.getMessageForLog(e));
+            log.debug(PSExceptionUtils.getDebugMessageForLog(e));
+            return Response.status(500).entity("A server error happened. Please try your request again.").type("text/plain").build();
+        }
+        return null;
     }
 }
