ed468b8955b5b00802da3d9faf407f57239e89dc
Vineet <vineetpeeyuse@gmail.com>
2022-01-24 18:52:41 +0530

    CMS-8728

--- projects/sitemanage/src/main/java/com/percussion/sitemanage/servlet/PSPreviewItemContent.java
+++ projects/sitemanage/src/main/java/com/percussion/sitemanage/servlet/PSPreviewItemContent.java
@@ -89,8 +89,7 @@ public class PSPreviewItemContent extends HttpServlet
         try
         {
             String type = request.getParameter("type");
-            if (requestUri.endsWith(".css") || requestUri.endsWith("favicon.ico"))
-                return;
+
             String url = createAssemblyUrl(requestUri, revision, type);
             
             HttpServletRequest forwardReq = getRequestFromUrl(url, request);
