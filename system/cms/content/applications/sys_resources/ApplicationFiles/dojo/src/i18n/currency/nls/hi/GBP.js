5930f9843563a5cade2f14ed1c29e7ef4f7363fb
sdhariwal <sonydeswal@yahoo.com>
2022-05-12 21:46:35 +0530

    Adding dojo

---
+++ system/cms/content/applications/sys_resources/ApplicationFiles/dojo/src/i18n/currency/nls/hi/GBP.js
@@ -0,0 +1,11 @@
+/*
+	Copyright (c) 2004-2006, The Dojo Foundation
+	All Rights Reserved.
+
+	Licensed under the Academic Free License version 2.1 or above OR the
+	modified BSD license. For more information on Dojo licensing, see:
+
+		http://dojotoolkit.org/community/licensing.shtml
+*/
+
+({"displayName":"ब�?रितन का पौन�?ड स�?टर�?लिग","symbol":"£"})
---
+++ system/cms/content/applications/sys_resources/ApplicationFiles/dojo/src/widget/nls/zh-cn/validate.js
@@ -0,0 +1,11 @@
+/*
+	Copyright (c) 2004-2006, The Dojo Foundation
+	All Rights Reserved.
+
+	Licensed under the Academic Free License version 2.1 or above OR the
+	modified BSD license. For more information on Dojo licensing, see:
+
+		http://dojotoolkit.org/community/licensing.shtml
+*/
+
+({"rangeMessage":"* 输入数�?�超出值域。","invalidMessage":"* �?�法的输入值。","missingMessage":"* 此值是必须的。"})
