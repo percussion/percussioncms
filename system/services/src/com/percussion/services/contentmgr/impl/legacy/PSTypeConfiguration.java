b3f6c54650c901fcc77543aeefcff3e71616536e
Nate Chadwick <nate.chadwick@gmail.com>
2022-07-21 12:30:50 -0400

    Clear some java warnings

--- system/services/src/com/percussion/services/contentmgr/impl/legacy/PSTypeConfiguration.java
+++ system/services/src/com/percussion/services/contentmgr/impl/legacy/PSTypeConfiguration.java
@@ -113,7 +113,7 @@ public class PSTypeConfiguration implements NodeType, Serializable
       /**
        * The sysid is used, as for child tables
        */
-      CHILDID;
+      CHILDID
    }
 
    /**
@@ -489,7 +489,7 @@ public class PSTypeConfiguration implements NodeType, Serializable
     */
    private static Map<String,Class> ms_hibernateTypeMap = 
       new HashMap<>();
-   /**
+   /*
     * Initialize type map
     */
    static {
@@ -514,7 +514,6 @@ public class PSTypeConfiguration implements NodeType, Serializable
     * @param isDerbyDatabase if <code>true</code> then the repository is a 
     *      derby database.
     */
-   @SuppressWarnings("unchecked")
    public PSTypeConfiguration(PSItemDefinition definition, PSItemChild child, 
          boolean isDerbyDatabase) 
    {
@@ -630,7 +629,6 @@ public class PSTypeConfiguration implements NodeType, Serializable
     *    
     * @return <code>true</code> the maps are equal. 
     */
-   @SuppressWarnings("unchecked")
    private boolean isEqualMap(Map<Class, ? extends Object> m1,
          Map<Class, ? extends Object> m2)
    {
@@ -658,7 +656,7 @@ public class PSTypeConfiguration implements NodeType, Serializable
     * @return the map value where the map key has the same base name as the 
     *    supplied key. It may be <code>null</code> if cannot find one in the map.
     */
-   @SuppressWarnings("unchecked")
+
    private Object getMapValue(Class srcKey, Map<Class, ? extends Object> m)
    {
       for (Class k : m.keySet())
@@ -1023,7 +1021,7 @@ public class PSTypeConfiguration implements NodeType, Serializable
 
       // Add to the implementing classes
       m_implementingClasses.add(new ImplementingClass(beanClass, hibConfig
-            .toString(), lazyFields));
+            , lazyFields));
       m_properties.put(beanClass, props);
       m_loadpolicy.put(beanClass, load);
    }
@@ -1179,7 +1177,7 @@ public class PSTypeConfiguration implements NodeType, Serializable
       if (ms_logger.isDebugEnabled())
       {
          ms_logger.debug("Hibernate config for class-name = \""
-               + beanClass.getName() + "\"\n" + hibConfig.toString());
+               + beanClass.getName() + "\"\n" + hibConfig);
       }
       return hibConfig.toString();
    }
@@ -1829,7 +1827,7 @@ public class PSTypeConfiguration implements NodeType, Serializable
    {
       loadPropertyDefinitions();
       Collection<PSPropertyDefinition> defs = m_propertyDefinitions.values();
-      PropertyDefinition rval[] = new PropertyDefinition[defs.size()];
+      PropertyDefinition[] rval = new PropertyDefinition[defs.size()];
       defs.toArray(rval);
       return rval;
    }
