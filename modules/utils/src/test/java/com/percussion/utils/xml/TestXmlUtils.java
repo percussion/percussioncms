86539df79d286662579f8c4f776c00c872f63d59
Nate Chadwick <nate.chadwick@gmail.com>
2022-08-10 11:19:59 -0400

    Add check to unescape xml if it is escaped prior to deserialization.

---
+++ modules/utils/src/test/java/com/percussion/utils/xml/TestXmlUtils.java
@@ -0,0 +1,45 @@
+/*
+ *     Percussion CMS
+ *     Copyright (C) 1999-2022 Percussion Software, Inc.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     Mailing Address:
+ *
+ *      Percussion Software, Inc.
+ *      PO Box 767
+ *      Burlington, MA 01803, USA
+ *      +01-781-438-9900
+ *      support@percussion.com
+ *      https://www.percussion.com
+ *
+ *     You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.percussion.utils.xml;
+
+import org.apache.commons.lang.StringEscapeUtils;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit tests for the xml utils class
+ */
+public class TestXmlUtils {
+
+    @Test
+    public void testXMLEncoded(){
+
+        assertFalse(PSXmlUtils.isStringXMLEscaped("<?xml version='1.0'?><test>text</test>"));
+        assertTrue(PSXmlUtils.isStringXMLEscaped(StringEscapeUtils.escapeXml("<?xml version='1.0'?><test>text</test>")));
+
+    }
+}
