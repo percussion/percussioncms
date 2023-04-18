d4383adff0aeb418c693858cf673b1d4c79b944a
Nate Chadwick <nate.chadwick@gmail.com>
2023-04-18 15:41:37 -0400

    Fixes issue #755, remove the CHACHA ciphers from the default list as it isn't supported by all supported 1.8 JRE's.

--- modules/utils/src/main/java/com/percussion/utils/container/PSCiphers.java
+++ modules/utils/src/main/java/com/percussion/utils/container/PSCiphers.java
@@ -65,8 +65,6 @@ public class PSCiphers
       modern_ciphers.add("TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384");
       modern_ciphers.add("TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256");
       modern_ciphers.add("TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256");
-      modern_ciphers.add("TLS_DHE_RSA_WITH_CHACHA20_POLY1305_SHA256");
-      modern_ciphers.add("TLS_CHACHA20_POLY1305_SHA256");
       modern_ciphers.add("TLS_DH_RSA_WITH_AES_128_GCM_SHA256");
       intermediate_ciphers.add("TLS_RSA_WITH_AES_128_GCM_SHA256");
       intermediate_ciphers.add("TLS_RSA_WITH_AES_256_GCM_SHA384");
