6a6ffab4b523c988d00aff1332cc10bb10ee2bfd
Nate Chadwick <nate.chadwick@intsof.com>
2023-10-19 14:31:14 -0400

    Add missing jaxb @XmlSeeAlso annotations for wrapper list classes.

--- rest/src/main/java/com/percussion/rest/acls/AclList.java
+++ rest/src/main/java/com/percussion/rest/acls/AclList.java
@@ -22,10 +22,12 @@ import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
 import java.util.ArrayList;
 import java.util.Collection;
 
 @XmlRootElement(name = "AclList")
+@XmlSeeAlso(Acl.class)
 @ArraySchema(schema = @Schema(implementation = Acl.class))
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class AclList extends ArrayList<Acl> {
